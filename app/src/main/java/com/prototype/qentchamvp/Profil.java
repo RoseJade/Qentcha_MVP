a799743293001912d43c544a7234c19e13abfb05
RoseJade <jade.rose@etu.univ-lehavre.fr>
2022-05-20 15:40:22 +0200

    Update de l'activité Profil

--- app/src/main/java/com/prototype/qentchamvp/Profil.java
+++ app/src/main/java/com/prototype/qentchamvp/Profil.java
@@ -4,18 +4,41 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class Profil extends AppCompatActivity {
+    ImageView img;
+    Button btnPhoto;
+
+    // constante
+    int SELECT_PICTURE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        // Vues
+        img = findViewById(R.id.photo);
+        btnPhoto = findViewById(R.id.btnSelectPhoto);
+
+        // Bouton du choix de l'image
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choisirImage();
+            }
+        });
+
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
@@ -53,4 +76,34 @@ public class Profil extends AppCompatActivity {
             }
         } );
     }
+
+    // Fonction appelée au clique du bouton image
+    void choisirImage() {
+        // Instance de type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // On compare la constante et la requête
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // Fonction appelé lors de la sélection de l'image de ChoisirImage
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // Comparaison
+            if (requestCode == SELECT_PICTURE) {
+                // On prend l'url de l'image
+                Uri imgUri = data.getData();
+                if (null != imgUri) {
+                    // on met à jour l'ancienne image dans le layout
+                    img.setImageURI(imgUri);
+                }
+            }
+        }
+    }
+
 }
