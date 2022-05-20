8efa9db560aa0f996bfdd2bc8a6bcd1033f6079d
ArthurBoulay <arthur.boulay@etu.univ-lehavre.fr>
2022-05-20 09:38:05 +0200

    Ajout du support de la localisation dans l'Activité Accueil.

--- app/src/main/java/com/prototype/qentchamvp/MainActivity.java
+++ app/src/main/java/com/prototype/qentchamvp/MainActivity.java
@@ -4,27 +4,45 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.location.Location;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Switch;
 
 // Google Maps
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 // Services de localisation
-import android.location.LocationManager;
+import android.location.*;
+import android.location.Location;
 import android.os.Bundle;
 import android.content.Context;
 
 
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity
+        implements LocationListener,
+        OnMyLocationButtonClickListener,
+        OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     private MapAccueil carte;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +53,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         FragmentManager fragmentManager = this.getSupportFragmentManager();
         this.carte = (MapAccueil) fragmentManager.findFragmentById(R.id.mapHome);
+
+        // SupportMapFragment nécéssaire pour le service de localisation
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapHome);
+        mapFragment.getMapAsync(this);
 
         // ===> Localisation <===
 
@@ -52,35 +75,66 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch ( menuItem.getItemId()) {
+                switch (menuItem.getItemId()) {
                     case R.id.action_carte:
                         return true;
 
                     case R.id.action_ajoutcarnet:
-                        startActivity( new Intent( getApplicationContext()
-                                ,AjoutCarnet.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , AjoutCarnet.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.action_moncarnet:
-                        startActivity( new Intent( getApplicationContext()
-                                ,MonCarnet.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , MonCarnet.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.action_profil:
-                        startActivity( new Intent( getApplicationContext()
-                                ,Profil.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , Profil.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
-        } );
+        });
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    // Ajout de la surcouche localisation à la carte
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        this.map.setOnMyLocationButtonClickListener(this);
+        this.map.setOnMyLocationClickListener(this);
+
+        // Verification de l'autorisation de récupérer la localisation
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
 
     }
 }
