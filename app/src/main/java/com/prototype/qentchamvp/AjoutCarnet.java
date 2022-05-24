ef900643590fcbf5f5ec4e7c066ce9736c36ca3b
ArthurBoulay <arthur.boulay@etu.univ-lehavre.fr>
2022-05-24 14:40:05 +0200

    Création d'un point selon la localisation de l'utilisateur.

--- app/src/main/java/com/prototype/qentchamvp/AjoutCarnet.java
+++ app/src/main/java/com/prototype/qentchamvp/AjoutCarnet.java
@@ -37,8 +37,13 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
 // Barre de Navigation
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Locale;
 
 public class AjoutCarnet extends AppCompatActivity
         implements LocationListener,
@@ -79,9 +84,9 @@ public class AjoutCarnet extends AppCompatActivity
 
         // ===> Ecouteurs pour les boutons <===
 
-        findViewById(R.id.btnVueGlobe)  .setOnClickListener(this);
+        findViewById(R.id.btnVueGlobe).setOnClickListener(this);
         findViewById(R.id.btnAjoutPoint).setOnClickListener(this);
-        findViewById(R.id.btnPrevue)    .setOnClickListener(this);
+        findViewById(R.id.btnPrevue).setOnClickListener(this);
 
         // Set carte selected
         bottomNavigationView.setSelectedItemId(R.id.action_ajoutcarnet);
@@ -90,48 +95,59 @@ public class AjoutCarnet extends AppCompatActivity
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch ( menuItem.getItemId()) {
+                switch (menuItem.getItemId()) {
                     case R.id.action_carte:
-                        startActivity( new Intent( getApplicationContext()
-                                ,MainActivity.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , MainActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.action_ajoutcarnet:
                         return true;
 
                     case R.id.action_moncarnet:
-                        startActivity( new Intent( getApplicationContext()
-                                ,MonCarnet.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , MonCarnet.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.action_profil:
-                        startActivity( new Intent( getApplicationContext()
-                                ,Profil.class) );
-                        overridePendingTransition(0,0);
+                        startActivity(new Intent(getApplicationContext()
+                                , Profil.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
-        } );
+        });
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btnVueGlobe)
-        {
+        if (view.getId() == R.id.btnVueGlobe) {
             this.map.animateCamera(CameraUpdateFactory.zoomTo(1));
         }
 
-        if (view.getId() == R.id.btnAjoutPoint)
-        {
-            Intent intent = new Intent(this, AjoutPoint.class);
-            startActivity(intent);
+        if (view.getId() == R.id.btnAjoutPoint) {
+            // Verification de l'autorisation de récupérer la localisation
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+
+            this.addPointOnLocation();
+
+            //Intent intent = new Intent(this, AjoutPoint.class);
+            //startActivity(intent);
         }
 
-        if (view.getId() == R.id.btnPrevue)
-        {
+        if (view.getId() == R.id.btnPrevue) {
             Intent intent = new Intent(this, PreviewTrajet.class);
             startActivity(intent);
         }
@@ -156,9 +172,8 @@ public class AjoutCarnet extends AppCompatActivity
             return;
         }
         this.map.setMyLocationEnabled(true);
-        //Location loc = fusedLocationClient.getLastLocation().getResult();
 
-        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(loc.getLatitude(), loc.getLongitude()), 10));
+        this.onActivityStart();
     }
 
     @Override
@@ -174,5 +189,57 @@ public class AjoutCarnet extends AppCompatActivity
     @Override
     public void onLocationChanged(@NonNull Location location) {
 
+    }
+
+    private void addPointOnLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+
+                    LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.addMarker(new MarkerOptions().position(point).title("Position"));
+
+
+                }
+            }
+        });
+    }
+
+    private void onActivityStart() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+
+                    LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.moveCamera(CameraUpdateFactory.newLatLng(point));
+
+
+                }
+            }
+        });
     }
 }
